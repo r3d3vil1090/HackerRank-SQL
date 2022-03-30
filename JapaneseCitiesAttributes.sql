b87cf6f1d394c4ec2ac4185d996c081a1b97fcb5
r3d3vil1090 <74372610+r3d3vil1090@users.noreply.github.com>
2022-03-29 19:59:39 -0600

    Create JapaneseCitiesAttributes.sql

---
+++ JapaneseCitiesAttributes.sql
@@ -0,0 +1,6 @@
+/*
+Task: Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+URL: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+*/
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN'
