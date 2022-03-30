a656a2e9fa3c5e6e31781d3a2262eae01b8171f2
r3d3vil1090 <74372610+r3d3vil1090@users.noreply.github.com>
2022-03-29 20:01:16 -0600

    Create WeatherObservationStation1.sql

---
+++ WeatherObservationStation1.sql
@@ -0,0 +1,7 @@
+/*
+Task: Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+
+URL: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+*/
+SELECT CITY, STATE FROM STATION
