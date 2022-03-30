d1404cafce040f1cfbe361cde7114195aaa4c547
r3d3vil1090 <74372610+r3d3vil1090@users.noreply.github.com>
2022-03-29 20:03:17 -0600

    Update WeatherObservationStation1.sql

--- WeatherObservationStation1.sql
+++ WeatherObservationStation1.sql
@@ -1,6 +1,5 @@
 /*
 Task: Query a list of CITY and STATE from the STATION table.
-The STATION table is described as follows:
 
 URL: https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
 */
